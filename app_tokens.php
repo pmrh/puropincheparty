e83e73140591e673bae1909ec7cbc7846d750842
Paul M.R. Harrison <paul.mr.harrison@gmail.com>
2015-08-24 23:43:38 -0700

    beem me up scotty

---
+++ app_tokens.php
@@ -0,0 +1,6 @@
+<?php
+$consumer_key = '64VZREyIATNfKw1NqVxjruBar';
+$consumer_secret = 'J7etjTWs6hBkJuTB9FeZAt8LfMsKS91HkKN24F7eVcfqczfQg8';
+$user_token = '399689911-Cf4f7F2MhgODsu9DyjpUhZ2YuK9ylcMgzMy69eFc';
+$user_secret = 'F2hKg7vMOq7hJTzoD5dT9yyIMS44Meg6fgyzp20kB2wGV';
+?>
